a71995209c2c803ec31908a4e07e2a81689e27a4
chenwenzhe <415711543@qq.com>
2020-04-26 18:11:15 +0800

    添加 404

--- .umirc.ts
+++ .umirc.ts
@@ -4,11 +4,15 @@ const fs = require('fs');
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath: any) =>
   path.resolve(appDirectory, relativePath);
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
+
 const appInfo = JSON.parse(fs.readFileSync(resolveApp('package.json')));
 
 const routes = [
   { exact: true, path: '/', redirect: '/user/login' },
-  { name: '登录页', path: '/user/login', component: '@/pages/user/login' },
+  { name: '登录页', path: '/user/login', component: '@/pages/User/Login' },
+  { name: '404', path: '/NotFound', component: '@/pages/Exception404' },
+  { path: '*', redirect: '/NotFound' },
 ];
 
 export default defineConfig({
@@ -20,4 +24,19 @@ export default defineConfig({
     { name: 'version', value: appInfo.version },
     { name: 'build_time', value: new Date() },
   ],
+  chainWebpack(config, { _webpack }) {
+    config.module
+      .rule('lint')
+      .test(/.(js|jsx|ts|tsx)$/)
+      .pre()
+      .include.add(resolveApp('src'))
+      .end()
+      // Even create named uses (loaders)
+      .use('eslint')
+      .loader(require.resolve('eslint-loader'))
+      .options({
+        formatter: eslintFormatter,
+        eslintPath: require.resolve('eslint'),
+      });
+  },
 });
---
+++ src/pages/Exception404/index.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-wrap-multilines */
+import { Link } from 'umi';
+import { Result, Button } from 'antd';
+import React from 'react';
+
+export default (): React.ReactNode => (
+  <Result
+    status="404"
+    title="404"
+    style={{
+      background: 'none',
+    }}
+    subTitle="Sorry, the page you visited does not exist."
+    extra={
+      <Link to="/">
+        <Button type="primary">Back Home</Button>
+      </Link>
+    }
+  />
+);
